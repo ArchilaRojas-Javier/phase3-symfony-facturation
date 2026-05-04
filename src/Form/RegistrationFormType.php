b4b17ea844ee5360f909fb20f3f379c21c9db78b
Javier <Javier@gmail.com>
2026-05-04 13:10:55 +0200

    register form created

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email as ConstraintsEmail;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,26 +21,44 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('first_name')
-        ->add('last_name')
-        ->add('company_name')
-        ->add('iban')
-        
-        ->add('email')
-        ->add('agreeTerms', CheckboxType::class, [
-            'mapped' => false,
-            'constraints' => [
-                new IsTrue(
-                    message: 'You should agree to our terms.',
-                ),
+        ->add('first_name', TextType::class, [
+            'label' => 'Prenom',
+            'attr' => [
+                'placeholder' => 'Jean',
             ],
         ])
+        ->add('last_name', TextType::class, [
+            'label' => 'Nom',
+            'attr' => [
+                'placeholder' => 'Dupont',
+            ],
+        ])
+        ->add('company_name', TextType::class, [
+            'label' => 'Raison Sociale',
+            'attr' => [
+                'placeholder' => 'Tech Solutions',
+            ],
+        ])
+        ->add('iban', TextType::class, [
+            'label' => 'IBAN',
+            'attr' => [
+                'placeholder' => 'FR76 3000 6000 0112 3456 7890 189',
+            ],
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Adresse Email',
+            'attr' => [
+                'placeholder' => 'contact@techsolutions.com',
+            ],
+        ])
+        
         
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password',
+                'placeholder' => '*******'],
                 'constraints' => [
                     new NotBlank(
                         message: 'Please enter a password',
